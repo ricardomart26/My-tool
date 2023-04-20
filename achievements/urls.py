1e02b6f3487e6019248ca3e04224157770c96af0
ricardomart26 <ricardomartins2626t@gmail.com>
2023-04-20 22:02:36 +0100

    Added url for each user

--- achievements/urls.py
+++ achievements/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login", views.login_user, name="login_user"),
     path("signup", views.signup_user, name="signup_user"),
     path("logout", views.logout_user, name="logout_user"),
+    path("user/<string:username>", views.get_user_by_username, name="username_view")
 ]
